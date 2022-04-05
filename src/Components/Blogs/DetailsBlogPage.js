391acac54a3542a190e72cffe3b376a45c1d1e85
md. moniruzzaman <md.moniruzzaman1050@gmail.com>
2022-04-05 13:13:48 +0600

    writed blogs question answer

--- src/Components/Blogs/DetailsBlogPage.js
+++ src/Components/Blogs/DetailsBlogPage.js
@@ -17,8 +17,8 @@ const details = blog.find(blg=> blg.id == idea)
 
     return (
         <div className='min-h-screen'>
-         <div className='flex justify-center mb-11'>
-             <img className='w-9/12  border-2 m-5  h-[600px]' src={details?.img} alt="" />
+         <div className='flex justify-center pb-14'>
+             <img className='w-9/12 h-auto border-2 m-5 ' src={details?.img} alt="" />
          </div>
          <div className='w-9/12 m-5 mx-auto mb-44'>
              <h1 className='text-3xl font-heading mb-11'>{details?.title}</h1>
--- src/Components/Blogs/SingleBlogCard.js
+++ src/Components/Blogs/SingleBlogCard.js
@@ -14,7 +14,7 @@ const SingleBlogCard = ({blogs}) => {
     return (
         <div >
          <div className="rounded overflow-hidden shadow-lg relative min-h-[510px] ">
-      <img className="w-full" src={img} alt="Mountain"/>
+      <img className="w-full " src={img} alt="Mountain"/>
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{title}</div>
         <p className="text-gray-700 text-base mb-11">
